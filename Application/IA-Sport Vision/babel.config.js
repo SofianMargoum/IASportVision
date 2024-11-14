bb168d4228cc9f92699533859c7cb13e463aed82
SofianMargoum <margoum.contact@gmail.com>
2024-11-14 13:13:29 +0100

    IASV update SPRINT 2 React native reanimed update OK

--- Application/IA-Sport Vision/babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin', // Assurez-vous que cela est en bas de la liste des plugins
-  ],
-};
--- Application/IA-Sport Vision/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
- 
-import { AppRegistry } from 'react-native';
-import App from './App'; // Assurez-vous que ce chemin pointe vers votre fichier App.js
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
